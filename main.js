96308bf6fa2abe2864a244a0eb5b521ff9c4ab36
istvan kosa <istvankosa@istvans-iMac.local>
2022-09-28 10:22:28 -0700

    Reformat to create books with classes

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 let myLibrary = [];
 
-function Book(title, author, pages, status) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.status = status
+// function Book(title, author, pages, status) {
+//   this.title = title
+//   this.author = author
+//   this.pages = pages
+//   this.status = status
+// }
+
+class Book {
+  constructor(title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+  }
 }
 
 function addBookToLibrary(book) {
